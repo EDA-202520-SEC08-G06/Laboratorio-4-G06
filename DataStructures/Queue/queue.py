0e84eba7c0485e676f99295cac11e0396b9512c0
NeuralKaizen <neuralkaizen@gmail.com>
2025-08-29 16:49:00 -0500

    TAD colas

---
+++ DataStructures/Queue/queue.py
@@ -0,0 +1,43 @@
+# COLAS 
+
+from DataStrucutres import array_list.py as list 
+from DataStrucutres import single_linked_list.py as list
+
+
+
+def new_queue ():
+
+    return list.new_list() 
+
+
+
+def size (my_queue) -> int: 
+
+    return list.size(my_queue)
+
+
+
+def is_empty (my_queue) -> bool:
+
+    return list.is_empty(my_queue)
+
+
+
+def enqueue (my_queue, element): 
+
+    return list.add_last(my_queue, element)
+
+
+
+def dequeue (my_queue):
+
+    return  list.remove_first(my_queue)
+
+
+
+def peek (my_queue): 
+
+    return list.first_element(my_queue) 
+        
+
+

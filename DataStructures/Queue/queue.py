2750077b2b022fe530af794a54930a865c53854e
NeuralKaizen <neuralkaizen@gmail.com>
2025-09-04 13:24:28 -0500

    correcion en queue.py

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -1,7 +1,7 @@
 # COLAS 
 
-from DataStrucutres import array_list as list 
-#from DataStrucutres import single_linked_list as list
+from DataStructures import array_list as list 
+#from DataStructures import single_linked_list as list
 
 
 

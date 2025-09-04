673338c09a97c3f550d175028cf281a0b873afae
NeuralKaizen <neuralkaizen@gmail.com>
2025-09-04 10:24:36 -0500

    stack.py (revisar)

--- DataStructures/Stack/stack.py
+++ DataStructures/Stack/stack.py
@@ -1,2 +1,43 @@
+# PILAS
+
+from DataStrucutres import array_list.py as list 
+from DataStrucutres import single_linked_list.py as list
+
+
+
 def new_stack():
-    return {}
+    
+    my_stack = list.new_list()
+
+    return my_stack
+
+
+
+def push(my_stack): 
+    
+    return  list.add_first(my_stack)
+
+
+
+def pop(my_stack):
+    
+    return list.remove_first(my_stack)
+
+
+
+def is_empty(my_stack): 
+    
+    return list.is_empty(my_stack) 
+
+
+
+def top(my_stack): 
+    
+    return list.first_element(my_stack)
+
+
+
+def size(my_stack): 
+    
+    return list.size(my_stack) 
+
